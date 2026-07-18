8f136e02de34cc24abd7334a0abe088c7b53ebb2
soynvn-byte <soyn.vn@gmail.com>
2026-07-18 10:14:57 +0700

    Use new_18 image for Art Clock article

--- assets/js/news-images.js
+++ assets/js/news-images.js
@@ -5,7 +5,7 @@
     "trang-tri-khong-gian-song-voi-tranh-canvas": "news/news_01.jpg",
     "tranh-treo-tuong-co-that-su-can-thiet": "news/news_04.jpg",
     "de-tranh-len-tieng-ve-gu-tham-my": "news/news_09.jpg",
-    "art-clock-khi-thoi-gian-tro-thanh-nghe-thuat": "news/news_02.jpg",
+    "art-clock-khi-thoi-gian-tro-thanh-nghe-thuat": "news/new_18.jpg",
     "suc-hut-cua-tranh-truu-tuong-toi-gian": "news/news_10.jpg",
     "mix-match-tranh-cho-goc-sofa": "news/news_07.jpg"
   };
