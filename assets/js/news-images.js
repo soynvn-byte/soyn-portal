4740119faa5616ca5eb9a4660a5a7b00c14d4d3f
soynvn-byte <soyn.vn@gmail.com>
2026-07-18 10:32:22 +0700

    Fix broken news image path and update story cover

--- assets/js/news-images.js
+++ assets/js/news-images.js
@@ -3,8 +3,8 @@
 
   const covers = {
     "trang-tri-khong-gian-song-voi-tranh-canvas": "news/news_01.jpg",
-    "tranh-treo-tuong-co-that-su-can-thiet": "news/news_04.jpg",
-    "de-tranh-len-tieng-ve-gu-tham-my": "news/news_09.jpg",
+    "tranh-treo-tuong-co-that-su-can-thiet": "news/new_04.jpg",
+    "de-tranh-len-tieng-ve-gu-tham-my": "news/news_16.jpg",
     "art-clock-khi-thoi-gian-tro-thanh-nghe-thuat": "news/new_18.jpg",
     "suc-hut-cua-tranh-truu-tuong-toi-gian": "news/news_10.jpg",
     "mix-match-tranh-cho-goc-sofa": "news/news_07.jpg"
